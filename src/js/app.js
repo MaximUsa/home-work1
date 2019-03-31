5bca4559e05c041c07bcedeb738ef72dfadd4f10
Maxim Usanov <max.usanov@gmail.com>
2019-03-31 21:11:04 +0300

    test add

--- src/js/app.js
+++ src/js/app.js
@@ -1,12 +1,12 @@
-function cashBackCalc(normal, regular, maximal) {
-    let normalBack = 1 / 100;
-    let regularBack = 5 / 100;
-    let maxBack = 30 / 100;
-    let cashNormal = normal * normalBack;
-    let cashRegular = regular * regularBack;
-    let cashMax = maximal * maxBack;
+function CashBackCalc(normal, regular, maximal) {
+    const normalBack = 1 / 100;
+    const regularBack = 5 / 100;
+    const maxBack = 30 / 100;
+    const cashNormal = normal * normalBack;
+    const cashRegular = regular * regularBack;
+    const cashMax = maximal * maxBack;
     return cashNormal + cashRegular + cashMax;
 }
 
-const totalBack = cashBackCalc(344, 434, 3443);
+const totalBack = CashBackCalc(344, 434, 3443);
 console.log(`Cash back amount is: ${totalBack} rubles`);
